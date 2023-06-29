abe484ddb539167bdd95eda9763a5d6817809f37
xiejiapeng <xiejiapeng@mail.shareintelli.com>
2023-06-29 14:30:53 +0800

    调整border

--- test.js
+++ test.js
@@ -6,14 +6,14 @@ const pdf = new TablePDF({
   rows: require("./data.js").slice(0),
   rowConfig: [ 
     { width: "40", key: "_index_", title: "序号", justify: "center" },
-    { width: "*", key: "pron", title: "Pron" },
-    { width: "*", key: "spell", title: "Spell" },
-    { width: "***", key: "excerpt", title: "Excerpt" },
-    { width: "46", key: "_checkbox_", title: "日期1", justify: "center" },
-    { width: "46", key: "_checkbox_", title: "日期2", justify: "center" },
-    { width: "46", key: "_checkbox_", title: "日期3", justify: "center" },
-    { width: "46", key: "_checkbox_", title: "日期4", justify: "center" },
-    { width: "46", key: "_checkbox_", title: "日期5", justify: "center" }
+    { width: "*", key: "pron", title: "Pron", justify: "center" },
+    { width: "*", key: "spell", title: "Spell", justify: "center" },
+    { width: "***", key: "excerpt", title: "Excerpt", justify: "center" },
+    { width: "46", title: "日期1", justify: "center" },
+    { width: "46", title: "日期2", justify: "center" },
+    { width: "46", title: "日期3", justify: "center" },
+    { width: "46", title: "日期4", justify: "center" },
+    { width: "46", title: "日期5", justify: "center" }
   ]
 });
 
