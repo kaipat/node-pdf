8eb03e1d2d95e2628ac9733c18c26ad0eebbd76d
xiejiapeng <xiejiapeng@mail.shareintelli.com>
2023-06-30 17:55:17 +0800

    基本完成

--- test.js
+++ test.js
@@ -6,15 +6,15 @@ new TablePDF({
   title: "いましめる @Jeni",
   rows: require("./data.js").slice(0),
   rowConfig: [
-    { width: "34", key: "_index_", title: "序号" },
+    { width: "34", key: "_index_", title: "序号", justify: "center" },
     { width: "*", key: "pron", title: "Pron" },
     { width: "*", key: "spell", title: "Spell" },
-    { width: "***", key: "excerpt", title: "Excerpt" },
-    { width: "30", title: "D1" },
-    { width: "30", title: "D2" },
-    { width: "30", title: "D3" },
-    { width: "30", title: "D4" },
-    { width: "30", title: "D5" }
+    { width: "****", key: "excerpt", title: "Excerpt" },
+    { width: "30", title: "D1", justify: "center" },
+    { width: "30", title: "D2", justify: "center" },
+    { width: "30", title: "D3", justify: "center" },
+    { width: "30", title: "D4", justify: "center" },
+    { width: "30", title: "D5", justify: "center" }
   ],
   output: path.resolve(__dirname, "output-1.pdf")
 });
