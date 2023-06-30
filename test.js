52b05e4e17fcecbf5c3a4808555eb5bbecdfa6b7
xiejiapeng <xiejiapeng@mail.shareintelli.com>
2023-06-30 14:36:06 +0800

    基本完成

--- test.js
+++ test.js
@@ -2,10 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const TablePDF = require("./lib/tablePDF");
 
-const pdf = new TablePDF({
-  rows: require("./data.js").slice(0),
+new TablePDF({
   title: "いましめる @Jeni",
-  rowConfig: [ 
+  rows: require("./data.js").slice(0),
+  rowConfig: [
     { width: "34", key: "_index_", title: "序号" },
     { width: "*", key: "pron", title: "Pron" },
     { width: "**", key: "spell", title: "Spell" },
@@ -15,7 +15,17 @@ const pdf = new TablePDF({
     { width: "30", title: "D3" },
     { width: "30", title: "D4" },
     { width: "30", title: "D5" }
-  ]
+  ],
+  output: path.resolve(__dirname, "output-1.pdf")
 });
 
-pdf.pipe(fs.createWriteStream(path.resolve(__dirname, "output.pdf")));
+new TablePDF({
+  title: "いましめる @Jeni",
+  rows: require("./data.js").slice(0),
+  rowConfig: [
+    { width: "*", key: "spell", title: "Spell" },
+    { width: "***", key: "excerpt", title: "Excerpt" }
+  ],
+  output: path.resolve(__dirname, "output-2.pdf")
+});
+
