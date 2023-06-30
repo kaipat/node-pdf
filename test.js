1041fd270c8bc3181073aef90e7ad79378411bbd
xiejiapeng <xiejiapeng@mail.shareintelli.com>
2023-06-30 11:34:18 +0800

    调整border

--- test.js
+++ test.js
@@ -5,15 +5,15 @@ const TablePDF = require("./lib/tablePDF");
 const pdf = new TablePDF({
   rows: require("./data.js").slice(0),
   rowConfig: [ 
-    { width: "40", key: "_index_", title: "序号", justify: "center" },
-    { width: "*", key: "pron", title: "Pron", justify: "center" },
-    { width: "**", key: "spell", title: "Spell", justify: "center" },
-    { width: "**", key: "excerpt", title: "Excerpt", justify: "center" },
-    { width: "36", title: "D1", justify: "center" },
-    { width: "36", title: "D2", justify: "center" },
-    { width: "36", title: "D3", justify: "center" },
-    { width: "36", title: "D4", justify: "center" },
-    { width: "36", title: "D5", justify: "center" }
+    { width: "34", key: "_index_", title: "序号" },
+    { width: "*", key: "pron", title: "Pron" },
+    { width: "**", key: "spell", title: "Spell" },
+    { width: "**", key: "excerpt", title: "Excerpt" },
+    { width: "30", title: "D1" },
+    { width: "30", title: "D2" },
+    { width: "30", title: "D3" },
+    { width: "30", title: "D4" },
+    { width: "30", title: "D5" }
   ]
 });
 
